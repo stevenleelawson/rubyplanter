f29d1ac1636609af754939526a41e9678bd6eda6
Steven Lawson <steven.lawson@healthsqyre.com>
2022-04-29 13:25:54 -0600

    added plant table which reference person

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_221158) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_29_190925) do
   create_table "people", force: :cascade do |t|
     t.string "email"
     t.string "name"
@@ -18,4 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_221158) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.integer "person_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_plants_on_person_id"
+  end
+
+  add_foreign_key "plants", "people"
 end
